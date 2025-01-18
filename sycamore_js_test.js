d08e2332152ed5db7cf89bf79f8acde47a441314
Okeibunor Emmanuel <okeibunoremma@gmail.com>
2025-01-18 10:28:51 +0100

    fix: some changes

--- sycamore_js_test.js
+++ sycamore_js_test.js
@@ -1,7 +1,6 @@
 function solution(T) {
   let maxZigzag = 0;
 
-  // prevDir: null for root, 'L' for came from left, 'R' for came from right
   function findZigzag(node, prevDir, turns) {
     if (!node) return;
 
@@ -9,26 +8,21 @@ function solution(T) {
 
     // Go left
     if (prevDir === "R") {
-      // Coming from right, going left is a turn
       findZigzag(node.l, "L", turns + 1);
     } else {
-      // Coming from left or root, going left is not a turn
       findZigzag(node.l, "L", turns);
     }
 
     // Go right
     if (prevDir === "L") {
-      // Coming from left, going right is a turn
       findZigzag(node.r, "R", turns + 1);
     } else {
-      // Coming from right or root, going right is not a turn
       findZigzag(node.r, "R", turns);
     }
   }
 
   if (!T) return 0;
 
-  // Start from root (no previous direction)
   findZigzag(T, null, 0);
 
   return maxZigzag;
